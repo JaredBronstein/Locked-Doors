e2c78f1edc0f7f3dfc13a9a7387e575da6ef7047
kruegerc96 <49258382+kruegerc96@users.noreply.github.com>
2020-03-05 11:30:40 -0600

    added audio script and more content to fmod project
    added script FmodPlayer and imported assets to fmod project

---
+++ Escape Room/Assets/Scripts/Audio/FmodPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FmodPlayer : MonoBehaviour
+{
+    private float distance = 0.05f;
+    private float Material;
+
+    void FixedUpdate()
+    {
+        MaterialCheck();
+        Debug.DrawRay(transform.position, Vector3.down * distance, Color.blue);
+    }
+
+    void MaterialCheck()
+    {
+        RaycastHit hit;
+        hit = Physics.Raycast(transform.position, Vector3.down, distance);
+
+        if (hit.collider)
+        {
+            if (hit.collider.tag == "Wood")
+                Material = 1f;
+            else if (hit.collider.tag == "Carpet")
+                Material = 2f;
+            else if (hit.collider.tag == "Stone")
+                Material = 3f;
+            else
+                Material = 1f;
+        }
+    }
+
+    void PlayFootstepsEvent(string path)
+    {
+        FMOD.Studio.EventInstance Footsteps = FMODUnity.RuntimeManager.CreateInstance(path);
+        Footsteps.setParameterByName("Material", Material);
+        Footsteps.start();
+        Footsteps.release();
+    }
+}
