6a6a1c6ea208e27c543fe66753f51526feb44876
DutaiStudios <44248998+DutaiStudios@users.noreply.github.com>
2020-04-10 16:19:19 -0400

    Bedroom/Basement Notes
    Basement and Bedroom journal placeholders added
    Short editor text function added

---
+++ Escape Room/Assets/Scripts/PlaceholderNoteInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceholderNoteInfo : MonoBehaviour
+{
+
+    [TextArea]
+    [Tooltip("")]
+    public string ReadableText;
+
+    [TextArea]
+    [Tooltip("")]
+    public string SpecialConsiderations;
+}
