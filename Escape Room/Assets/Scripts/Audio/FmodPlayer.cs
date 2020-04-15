abf81b901e9edfe4a9f61757b349d355ae3e026a
kruegerc96 <49258382+kruegerc96@users.noreply.github.com>
2020-04-15 13:09:41 -0500

    Changed location of script, edited it
    Changed location of FmodPlayer.cs to player prefab and began work on increasing its functionality

--- Escape Room/Assets/Scripts/Audio/FmodPlayer.cs
+++ Escape Room/Assets/Scripts/Audio/FmodPlayer.cs
@@ -1,20 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FmodPlayer : MonoBehaviour
 {
     // ckrueger audio
 
+    FMOD.Studio.EventInstance Footsteps;
+
     public GameObject player;
 
     private bool isMoving;
+    private int floorMaterial; // 1f = wood, 2f = carpet, 3f = stone
+    private string sceneName;
 
     Vector3 lastPos;
     Vector3 currentPos;
 
     void Start()
     {
+        sceneName = SceneManager.GetActiveScene().name;
+
+        Footsteps = FMODUnity.RuntimeManager.CreateInstance("event:/Footsteps");
+
+        SetFloorMaterial();
+
         player = GameObject.FindGameObjectWithTag("Player");
         isMoving = false;
     }
@@ -47,5 +58,35 @@ public class FmodPlayer : MonoBehaviour
     private void FootstepSound()
     {
         FMODUnity.RuntimeManager.PlayOneShot("event:/Footsteps", gameObject.GetComponent<Transform>().position);
+    }
+
+    // determines footstep sound based on scene name
+    private void SetFloorMaterial()
+    {
+        switch (sceneName)
+        {
+            case "Bedroom":
+                floorMaterial = 2;
+                //Debug.Log("Material Set: Bedroom");
+                break;
+
+            case "Foyer":
+                floorMaterial = 1;
+                //Debug.Log("Material Set: Foyer");
+                break;
+
+            case "Basement":
+                floorMaterial = 3;
+                //Debug.Log("Material Set: Basement");
+                break;
+
+            default:
+                floorMaterial = 2;
+                //Debug.Log("Material Set: Default");
+                break;
+        }
+
+        Footsteps.setParameterByName("Material", floorMaterial);
+        Debug.Log("Material Parameter Set");
     }
 }
